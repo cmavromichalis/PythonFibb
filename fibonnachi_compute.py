f27188fe1abb2ee16a9123906798a03982626243
Constantine Mavromichalis <constantine.mavromichalis@macys.com>
2017-01-15 15:09:53 -0700

    Corrected socker_server to socket_server Kept hitting "Process finished with exit code 137 (interrupted by signal 9: SIGKILL)" when calculating and saving Fibonacchi values over 5000000. The requirements don't say to save it and given the time contraint just changing this to calculate it each time.

--- fibonnachi_compute.py
+++ fibonnachi_compute.py
@@ -1,24 +1,15 @@
 import time
 
-# Hold the numbers we calculated already; has our seeds to start
-FIBONACCI_NUMBERS = [0, 1]
-
-
 # Function that will calculate and return the n'th Fibonacci number.
 def fibonacci_generator(number):
     # Validate input
     if number <= 0:
-        return 'Please enter a number greater than 0'
-
-    # If we have already calculated this, return it
-    if len(FIBONACCI_NUMBERS) > number:
-        return FIBONACCI_NUMBERS[number]
+        return 0
 
     # Else calculate it where we left off
-    number_computed = len(FIBONACCI_NUMBERS)
-    a = FIBONACCI_NUMBERS[number_computed - 2]
-    b = FIBONACCI_NUMBERS[number_computed - 1]
-    counter = number_computed - 1
+    a = 0
+    b = 1
+    counter = 1
 
     # Calculation loop
     start_time = time.time()
@@ -29,17 +20,10 @@ def fibonacci_generator(number):
         # b += a
         # a = old_b
         counter += 1
-        FIBONACCI_NUMBERS.append(b)
     end_time = time.time()
+    print("Calculated Fibonnachi number", number, ":", b)
     print("Total time taken to calculate loop: ", end_time - start_time)
     return b
 
-
-print("Precalculating 10000", fibonacci_generator(10000))  # Error
-#print("Precalculating 10000", fibonacci_generator(11))  # Error
-#print("1st num should be. ", fibonacci_generator(10))  # 0
-#print("10 num should be. ", fibonacci_generator(10))  # 0
-#print("20 num should be. ", fibonacci_generator(20))  # 0
-#print("10 num should be. ", fibonacci_generator(10))  # 0
-# print("42 num should be. ", fibonacci_generator(ct.value))  # 0
-#print("500000 num should be.", fibonacci_generator(1000001))
+# The first 10000 is pretty quick; let's front load that.
+#fibonacci_generator(500000)
--- socket_server.py
+++ socket_server.py
@@ -11,16 +11,16 @@ class Handler(asyncore.dispatcher_with_send):
 
         if len(self.out_buffer) > 0:
             received_number = int.from_bytes(self.out_buffer, byteorder='little', signed=False)
-            print('Received number: ', received_number)
             number = fibonacci_generator(received_number)
-            print('Calculated Fibonnachi number: ', number)
             fibonnachi_number_bytes = number.to_bytes(((number.bit_length() + 7) // 8), byteorder='little',
                                                       signed=False)
-            print('Calculated Fibonnachi number: ', fibonnachi_number_bytes)
+            print('Received number: ', received_number)
+            print('Calculated Fibonnachi number: ', number)
+            print('Calculated Fibonnachi number bytes: ', fibonnachi_number_bytes)
 
+            # Grab chunks of bytes and transmit them until there are no more to grab
             i = 0
             byte_chunk = fibonnachi_number_bytes[i:i + 1024]
-            # Grab chunks of bytes and transmit them until there are no more to grab
             while byte_chunk != b'':
                 i += 1024
                 self.send(byte_chunk)
@@ -31,11 +31,11 @@ class Handler(asyncore.dispatcher_with_send):
 
 class FibonnachiServer(asyncore.dispatcher):
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, listener_number):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.bind((host, port))
-        self.listen(5)
+        self.listen(listener_number)
 
     # When we get a client connection, handle it with a dispatcher
     def handle_accept(self):
@@ -45,5 +45,5 @@ class FibonnachiServer(asyncore.dispatcher):
             print('Connection by ', address)
             Handler(socket)
 
-server = FibonnachiServer('localhost', 8080)
+server = FibonnachiServer('localhost', 8080, 5)
 asyncore.loop()
